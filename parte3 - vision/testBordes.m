1d9c99e90e614fef1aee9e694bbbe7e4d31484f4
mregueira <mregueira@itba.edu.ar>
2020-12-15 17:30:57 -0300

    hoja derecha

--- parte3 - vision/testBordes.m
+++ parte3 - vision/testBordes.m
@@ -10,30 +10,12 @@ imth = im_aux>0.5;
 figure();
 idisp(imth)
 
-% Deteccion por color verde
-% template0=imth(560:748,916:1110);
-% figure();
-% cerodetect=isimilarity(template0,imth);
-% mesh(cerodetect)
-% figure
-% idisp(cerodetect)
+im_size=size(im_aux);
 
+% Deteccion de bordes hoja
 im_marco = icanny(im_orig,1,0.4,1);
 figure();
 idisp(im_marco)
-
-% Deteccion de bordes en figura
-% K=ksobel();
-% imbordeh=iconv(im_inv,K);
-% imbordev=iconv(im_inv,K');
-% imbordenorm=((imbordeh).^2+(imbordev).^2).^0.5;
-% figure();
-% idisp(imbordenorm)
-
-% Filtro con threshold
-% imedge=imbordenorm<0.01;
-% figure();
-% idisp(imedge)
 
 % Identificacion de lineas
 imlin=Hough(im_marco);
@@ -108,10 +90,144 @@ posi_b=zeros(2,4);
 posi_b(2,:)=fil_b;
 posi_b(1,:)=col_b;
 
-posf_b=[1100 1 1100 1;1 1 1381 1381];
+% Ordenado de los puntos en estructura C
+posi_b = order_points(posi_b(1,:),posi_b(2,:),1381,1100);
+
+% Esto deberia ser el tamaño de bordescartel
+posf_b=[1381 1 1 1381;1 1 1100 1100]; 
 
 % Con esto la llevo bordes al plano
 matH_b=homography(posi_b,posf_b);
 warped_b=homwarp(matH_b,warpedth,'full');
 warpedth_b=warped_b>0.5;
 idisp(warpedth_b)
+close all;
+
+%% Recorto la hoja (provisorio hecho a mano)
+im_hoja = warpedth_b(557:1641,346:1716);
+warp_hoja = im_hoja;
+figure();
+idisp(warp_hoja)
+im_hoja = ~im_hoja;
+figure();
+idisp(im_hoja)
+size_hoja=size(im_hoja);
+
+%% Buscar lineas de bordes posta
+% Separo en cuadrantes (menos probable que mire el triangulo)
+im_q1 = im_hoja(1:(size_hoja(1)/4),(3*size_hoja(2)/4):size_hoja(2));
+im_q2 = im_hoja(1:(size_hoja(1)/2),1:(size_hoja(2)/2));
+im_q3 = im_hoja((size_hoja(1)/2):size_hoja(1),1:(size_hoja(2)/2));
+im_q4 = im_hoja((3*size_hoja(1)/4):size_hoja(1),(3*size_hoja(2)/4):size_hoja(2));
+
+hoja_borders = [0 0 0 0;0 0 0 0];
+
+
+% Regular threshold hasta que haya al menos 2 lineas
+% Despues con el suppress se recorta lo que encontro
+% Q2
+imlin_q2=Hough(im_q2);
+figure();
+idisp(im_q2)
+imlin_q2.houghThresh=0.9;
+imlin_q2.plot
+imlin_q2.suppress = 15;
+figure();
+idisp(im_q2)
+imlin_q2.plot
+lineas_q2=imlin_q2.lines;
+
+imlinea1_q2=generarlinea(lineas_q2(1).rho,lineas_q2(1).theta,size(im_q2,2),size(im_q2,1));
+imlinea2_q2=generarlinea(lineas_q2(2).rho,lineas_q2(2).theta,size(im_q2,2),size(im_q2,1));
+
+figure();
+idisp(im_q2.*imlinea1_q2+im_q2.*imlinea2_q2)
+
+bordes_q2=(imlinea1_q2+imlinea2_q2)==2;
+[fil_q2,col_q2]=find(bordes_q2);
+hoja_borders(:,2) = [col_q2;fil_q2]; % Recordar que va u - v
+
+% Q1 - Aca tome un cuadrante menor, chequear con las lineas si hay que
+% achicarlo
+imlin_q1=Hough(im_q1);
+figure();
+idisp(im_q1)
+imlin_q1.houghThresh=0.6; % Lo baje de a 0.1 hasta encontrar con los dos bordes
+imlin_q1.plot
+imlin_q1.suppress = 25; % Lo subi de a 5 una vez que hubo una linea de cada
+figure();
+idisp(im_q1)
+imlin_q1.plot
+lineas_q1=imlin_q1.lines;
+
+imlinea1_q1=generarlinea(lineas_q1(1).rho,lineas_q1(1).theta,size(im_q1,2),size(im_q1,1));
+imlinea2_q1=generarlinea(lineas_q1(2).rho,lineas_q1(2).theta,size(im_q1,2),size(im_q1,1));
+
+figure();
+idisp(im_q1.*imlinea1_q1+im_q1.*imlinea2_q1)
+
+bordes_q1=(imlinea1_q1+imlinea2_q1)==2;
+[fil_q1,col_q1]=find(bordes_q1);
+hoja_borders(:,1) = [col_q1+(3*size_hoja(2)/4);fil_q1];
+
+% Q3 - En este como en el 2 no hubo problema
+imlin_q3=Hough(im_q3);
+figure();
+idisp(im_q3)
+imlin_q3.houghThresh=0.9; 
+imlin_q3.plot
+imlin_q3.suppress = 15; 
+figure();
+idisp(im_q3)
+imlin_q3.plot
+lineas_q3=imlin_q3.lines;
+
+imlinea1_q3=generarlinea(lineas_q3(1).rho,lineas_q3(1).theta,size(im_q3,2),size(im_q3,1));
+imlinea2_q3=generarlinea(lineas_q3(2).rho,lineas_q3(2).theta,size(im_q3,2),size(im_q3,1));
+
+figure();
+idisp(im_q3.*imlinea1_q3+im_q3.*imlinea2_q3)
+
+bordes_q3=(imlinea1_q3+imlinea2_q3)==2;
+[fil_q3,col_q3]=find(bordes_q3);
+hoja_borders(:,3) = [col_q3;fil_q3+(size_hoja(1)/2)];
+
+% Q4 - Achique el cuadrante nuevamente pq me tomaba el triangulo
+imlin_q4=Hough(im_q4);
+figure();
+idisp(im_q4)
+imlin_q4.houghThresh=0.8; % Tambien lo baje pq no encontraba en la otra direccion
+imlin_q4.plot
+imlin_q4.suppress = 15; % Nota que si ya hay una sola de cada, no hace efecto
+figure();
+idisp(im_q4)
+imlin_q4.plot
+lineas_q4=imlin_q4.lines;
+
+imlinea1_q4=generarlinea(lineas_q4(1).rho,lineas_q4(1).theta,size(im_q4,2),size(im_q4,1));
+imlinea2_q4=generarlinea(lineas_q4(2).rho,lineas_q4(2).theta,size(im_q4,2),size(im_q4,1));
+
+figure();
+idisp(im_q4.*imlinea1_q4+im_q4.*imlinea2_q4)
+
+bordes_q4=(imlinea1_q4+imlinea2_q4)==2;
+[fil_q4,col_q4]=find(bordes_q4);
+hoja_borders(:,4) = [col_q4+(3*size_hoja(2)/4);fil_q4+(3*size_hoja(1)/4)];
+
+% Redondeo
+hoja_borders = round(hoja_borders);
+
+%% Ahora puedo hacer la transformacion final
+% Esto deberia ser el tamaño de bordescartel
+posf_hoja=[size_hoja(2) 1 1 size_hoja(2);1 1 size_hoja(1) size_hoja(1)]; 
+
+matH_h=homography(hoja_borders,posf_hoja);
+warped_h=homwarp(matH_h,warp_hoja,'full');
+warpedth_h=warped_h>0.5;
+idisp(warpedth_h)
+close all
+% Focus de la hoja (provisorio manual)
+size_hoja_final = size(warpedth_h);
+hoja_final = warpedth_h(120:size_hoja_final(1),292:size_hoja_final(2));
+figure();
+idisp(hoja_final)
