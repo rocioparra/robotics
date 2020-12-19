1b8c491664fb2ca7a6433ad06f92f208774d451e
amartorell <amartorell@itba.edu.ar>
2020-12-19 00:17:12 -0300

    generalizando cosas

---
+++ parte3 - vision/get_quadrant.m
@@ -0,0 +1,14 @@
+function [outputArg1,outputArg2] = get_quadrant(image)
+%GET_QUADRANT funcion que ubica cuatro cuadrantes donde se encuentran 
+%   
+    %obtengo max u y  max v 
+    aux=size(image);
+    u_max=aux(2);
+    v_max=aux(1);
+    
+    
+
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
