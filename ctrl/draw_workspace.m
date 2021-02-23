7c09ef012e12f6b28fbc0712f9cbb7f02af31c51
valenlago <vlago@itba.edu.ar>
2021-02-23 12:39:26 -0300

    actualizo draw_workspace

--- ctrl/draw_workspace.m
+++ ctrl/draw_workspace.m
@@ -12,10 +12,24 @@ function  draw_workspace(Robot, qlim, n)
             q(k) = qlim(k,1) + (qlim(k,2) - qlim(k,1)) * rand(1,1);
         end
     point = Robot.fkine(q);
-    Robot.plot(q);
-    hold on;
-    plot3(point.t(1),point.t(2),point.t(3),'b.','MarkerSize', 3);
+    c = jet;
+    xmax = 0.7;
+    xmin = -0.7;
+    xrange = xmax - xmin;
+    %mapeo la coordenada x por un numero [0;64] que es la cantidad de colores que tiene jet
+    color_index = round((point.t(1)-xmin)*64/xrange); 
+        
+        %if (point.t(2) >= -0.05) && (point.t(2) <= 0.05) 
+        % si se quiere dibujar una rebanada del espacio de trabajo, descomentar este if
+            hold on;
+            plot3(point.t(1),point.t(2),point.t(3),'b.','MarkerSize', 3, 'color', c(color_index,:) );
+        %end
     end
+    q0 = [0 0 0 0 0];
+    Robot.plot(q0);
+    hold on;
+    plot_box(0.45,-0.1,0.45-0.15,0.1,'linewidth', 3)
 end
+
 
     
